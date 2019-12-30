4ce532eb4cccf26130f4e4fe7d5354b881677320
Remi0110 <rmi.espin@gmail.com>
2019-12-30 16:37:14 +0100

    Programme de traitement des symptomes
    * Ajout classe MainProgram
    * Modification nom méthode
    * Update README.md

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,52 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+
+
+	ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile();
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+	/**
+	 * read symptoms in a file and create a list of symptoms 
+	 *
+	 * @param file
+	 * 
+	 * @return list of symptoms
+	 * 
+	 * @author Rémi ESPIN
+	 */
+	public List<String> reading(String file) {
+		return readSymptomDataFromFile.getSymptoms(file);
 	}
+	
+	/**
+	 * count occurences of symptoms in a list and create a map of symptoms 
+	 *
+	 * @param list of symptoms
+	 * 
+	 * @return map of iteration of symptoms with their occurences
+	 * 
+	 * @author Rémi ESPIN
+	 */
+	public Map<String, Integer> count(List<String> symptoms){
+		return readSymptomDataFromFile.getSymptomsWithOccurences(symptoms);
+	}
+	
+	/**
+	 * save a new file from a map of symptoms
+	 *
+	 * @param map with symptoms and their occurences
+	 * 
+	 * @return file with number of iteration of each symptoms from the map in param
+	 *        
+	 * @author Rémi ESPIN
+	 */
+	public void saving(Map<String, Integer> mapSymptomsOccurences) throws Exception {
+		readSymptomDataFromFile.writeSymptomsAndOccurences(mapSymptomsOccurences);
+	}
+
 }
+
+
