9b2589053a5897d63a8dcf4452f1b69f112535dc
Remi0110 <rmi.espin@gmail.com>
2019-12-30 16:37:14 +0100

    sorted map

--- src/com/hemebiotech/analytics/ISymptomReader.java
+++ src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,20 +1,45 @@
 package com.hemebiotech.analytics;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
- * Anything that will read symptom data from a source
- * The important part is, the return value from the operation, which is a list of strings,
- * that may contain many duplications
- * 
+ * Anything that will read symptom data from a source The important part is, the
+ * return value from the operation, which is a list of strings, that may contain
+ * many duplications
+ *
  * The implementation does not need to order the list
- * 
+ *
  */
 public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 *
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates
+	 *         are possible/probable
 	 */
-	List<String> GetSymptoms ();
+	List<String> getSymptoms(String fileName);
+
+	/**
+	 * count occurences of symptoms in a file
+	 *
+	 * @param file
+	 *            name
+	 * @return list of iteration of symptoms with their occurences : : :
+	 * @author Rémi ESPIN
+	 */
+	public Map<String, Integer> getSymptomsWithOccurences(String fileName);
+
+	/**
+	 * write a new file from a list of symptoms
+	 *
+	 * @param file
+	 *            name
+	 * @return file with number of iteration of symptoms from the file in argument :
+	 *         : :
+	 * @author Rémi ESPIN
+	 */
+	FileWriter writeFileFromListWithSymptomsAndOccurences(String fileName) throws IOException;
 }
--- src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -2,9 +2,15 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Simple brute force implementation
@@ -13,24 +19,30 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+
 	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 *
+	 * @param filepath
+	 *            a full or partial path to file with symptom strings in it, one per
+	 *            line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
+	public ReadSymptomDataFromFile(String filepath) {
 		this.filepath = filepath;
 	}
-	
+
+	public ReadSymptomDataFromFile() {
+
+	}
+
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
+	public List<String> getSymptoms(String fileName) {
+		List<String> result = new ArrayList<String>();
+
+		if (fileName != null) {
 			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				BufferedReader reader = new BufferedReader(new FileReader(fileName));
 				String line = reader.readLine();
-				
+
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
@@ -40,8 +52,40 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return result;
+	}
+
+	@Override
+	public FileWriter writeFileFromListWithSymptomsAndOccurences(String fileName) throws IOException {
+		Map<String, Integer> symptomsOccurences = this.getSymptomsWithOccurences(fileName);
+		FileWriter writer = new FileWriter("result.out");
+		symptomsOccurences.forEach((k, v) -> {
+			try {
+				writer.write(k + ":" + v);
+				writer.write(System.getProperty("line.separator"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+		writer.close();
+		return writer;
+	}
+
+	@Override
+	public Map<String, Integer> getSymptomsWithOccurences(String fileName) {
+		List<String> symptoms = this.getSymptoms(fileName);
+		Collections.sort(symptoms);
+		Map<String, Integer> map = new HashMap<>();
+
+		for (String symptom : symptoms) {
+			map.put(symptom, map.containsKey(symptom) ? map.get(symptom) + 1 : 1);
+		}
+
+		Map<String, Integer> sortedMap = new TreeMap<>(map);
+		return sortedMap;
+
 	}
 
 }
