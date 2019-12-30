f94273a330943836bd38d758a92aa388c7136606
Remi0110 <rmi.espin@gmail.com>
2019-12-30 16:37:14 +0100

    programme de traitement des symptomes
    * Ajout classe MainProgram
    * Modification nom méthode
    * Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Sample project for OpenClassroom's fundamentals of Java
+## Titre du projet
+Débugger une application JAVA
+
+## Contexte
+Le programme sur lequel j'ai travaillé est un programme d'analyse des tendances. Il a pour objectif de lire un fichier composé de plusieurs symptômes (symptoms.txt) et d'en écrire un nouveau (result.out), énumérant par ordre alphabétique les symptômes ainsi que leur nombre d'occurrences.
+
+## Objectifs et RÃ©alisation
+L'objectif était de débugger une application, à partir d'un existant, en implémentant une solution ayant recours au concept de la programmation orientée objet.
+
+## Exécution
+A l'exécution du programme, vous retrouverez le fichier de sortie (result.out) sur votre poste en suivant le chemin suivant:
+/Users/NameUser/result.out
+
+## Lien du projet initial
+https://github.com/OpenClassrooms-Student-Center/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/tree/master/Project02Eclipse
+
+## FabriquÃ© avec
+IDE : STS(Spring Tool Suit) 
+Langage: JAVA 8
--- src/com/hemebiotech/analytics/ISymptomReader.java
+++ src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,20 +1,45 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
- * Anything that will read symptom data from a source
- * The important part is, the return value from the operation, which is a list of strings,
- * that may contain many duplications
- * 
+ * Anything that will read symptom data from a source The important part is, the
+ * return value from the operation, which is a list of strings, that may contain
+ * many duplications
+ *
  * The implementation does not need to order the list
- * 
+ *
  */
 public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 *
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates
+	 *         are possible/probable
 	 */
-	List<String> GetSymptoms ();
+	List<String> getSymptoms(String fileName);
+
+	/**
+	 * count occurences of symptoms in a file
+	 *
+	 * @param list of symptoms
+	 * 
+	 * @return map of iteration of symptoms with their occurences
+	 * 
+	 * @author Rémi ESPIN
+	 */
+	public Map<String, Integer> getSymptomsWithOccurences(List<String> symptoms);
+
+	/**
+	 * write a new file from a map of symptoms
+	 *
+	 * @param map with symptoms and their occurences
+	 * 
+	 * @return file with number of iteration of each symptoms from the map in param
+	 *        
+	 * @author Rémi ESPIN
+	 */
+	void writeSymptomsAndOccurences(Map<String, Integer> mapSymptomsOccurences) throws IOException;
 }
