6061b3f5ada554b17414c18e61fe911bb63d5e2f
Remi0110 <rmi.espin@gmail.com>
2019-12-30 16:37:14 +0100

    programme de traitement des symptomes

--- src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -2,35 +2,25 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-/**
- * Simple brute force implementation
- *
- */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
-	
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
+	public List<String> getSymptoms(String fileName) {
+		List<String> result = new ArrayList<String>();
+
+		if (fileName != null) {
 			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				BufferedReader reader = new BufferedReader(new FileReader(fileName));
 				String line = reader.readLine();
-				
+
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
@@ -40,8 +30,52 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return result;
+	}
+	
+
+	@Override
+	public void writeSymptomsAndOccurences(Map<String, Integer> mapSymptomsOccurences) throws IOException {
+
+		String currentUsersHomeDir = System.getProperty("user.home");
+		String path = currentUsersHomeDir + System.getProperty("file.separator") + "result.out";
+		FileWriter writer = new FileWriter(path);
+		if (mapSymptomsOccurences != null && !mapSymptomsOccurences.isEmpty()) {
+			mapSymptomsOccurences.forEach((k, v) -> {
+				try {
+					writer.write(k + "=" + v);
+					writer.write(System.getProperty("line.separator"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+
+			writer.close();
+
+		} else {
+			writer.write("aucun symptome n'a été trouvé");
+			writer.close();
+		}
+		System.out.println("le fichier de sortie se trouve: " + path);
+	}
+
+	
+	@Override
+	public Map<String, Integer> getSymptomsWithOccurences(List<String> symptoms) {
+
+		Map<String, Integer> map = new TreeMap<>();
+
+		if (symptoms != null && !symptoms.isEmpty()) {
+			Collections.sort(symptoms);
+
+			for (String symptom : symptoms) {
+				map.put(symptom, map.containsKey(symptom) ? map.get(symptom) + 1 : 1);
+			}
+
+		}
+		return map;
+
 	}
 
 }
